82923ad97aef60c426101502743e2882a4801760
PrimeChuJiang <1349511491@qq.com>
2025-07-01 20:21:12 +0800

    水体制作完成

--- Scripts/Water/WaterBody.gd
+++ Scripts/Water/WaterBody.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var water_polygon: Polygon2D = $WaterPolygon
 @onready var water_spring : PackedScene = preload("res://Scenes/Water/WaterSpring.tscn")
+@onready var water_border: SmoothPath = $WaterBorder
+
 
 @export var k = 0.015
 @export var d = 0.03
@@ -11,6 +13,8 @@ extends Node2D
 # 弹簧个数
 @export var spring_number : int = 6
 @export var depth : float = 1000
+# 水面横截面宽度
+@export var border_thickness = 1.1
 
 var target_height = global_position.y
 var bottom = target_height + depth
@@ -18,14 +22,18 @@ var springs : Array[WaterSpring] = []
 var passes : float = 8
 
 func _ready() -> void:
+	water_border.width = border_thickness
+	
 	for i in range(spring_number):
 		var x_position : float = distance_between_springs * i
 		var w : WaterSpring = water_spring.instantiate()
 		add_child(w)
 		springs.append(w)
-		w.initialize(x_position)
-			
-	splash(2, 5)
+		w.initialize(x_position, i)
+		w.set_collision_width(distance_between_springs)
+		w.splash.connect(splash)
+		
+	#splash(2, 5)
 
 func _physics_process(delta: float) -> void:
 	for item in springs:
@@ -45,6 +53,7 @@ func _physics_process(delta: float) -> void:
 			if i < springs.size()-1:
 				right_deltas[i] = spread * (springs[i].height - springs[i+1].height)
 				springs[i+1].velocity += right_deltas[i]
+	new_border()
 	draw_water_body()
 
 func splash(index, speed):
@@ -52,18 +61,31 @@ func splash(index, speed):
 		springs[index].velocity += speed
 
 func draw_water_body():
-	var surface_points = []
-	for i in range(springs.size()):
-		surface_points.append(springs[i].position)
+	var curve : Curve2D = water_border.curve
+	var points = Array(curve.get_baked_points())
 	
+	var water_polygon_points = points
+
 	var first_index = 0
 	var last_index = springs.size()-1
 	
-	var water_polygon_points = surface_points
 	
-	water_polygon_points.append(Vector2(surface_points[last_index].x, bottom))
-	water_polygon_points.append(Vector2(surface_points[first_index].x, bottom))
+	water_polygon_points.append(Vector2(water_polygon_points[last_index].x, bottom))
+	water_polygon_points.append(Vector2(water_polygon_points[first_index].x, bottom))
 	
 	water_polygon_points = PackedVector2Array(water_polygon_points)
 	
 	water_polygon.set_polygon(water_polygon_points)
+
+func new_border():
+	var curve : Curve2D = Curve2D.new().duplicate()
+	var surface_points : Array[Vector2] = []
+	for i in range(springs.size()):
+		surface_points.append(springs[i].position)
+	
+	for i in range(surface_points.size()):
+		curve.add_point(surface_points[i])
+	
+	water_border.curve = curve
+	water_border.smooth(true)
+	water_border.queue_redraw()
--- Scripts/Water/WaterSpring.gd
+++ Scripts/Water/WaterSpring.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 class_name WaterSpring
 
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+
 # 弹簧当前的速度
 var velocity : float = 0
 
@@ -9,8 +11,15 @@ var force : float = 0
 
 var height : float = 0
 
+var index = 0
+
+var motion_factor = 0.01
+
+signal splash
 # 弹簧的自然位置
 var target_height : float = 0
+
+var collided_with = null
 
 func water_update(sprint_constant : float, dampening : float):
 	# 更新当前帧点位置
@@ -28,8 +37,28 @@ func water_update(sprint_constant : float, dampening : float):
 	#print(velocity)
 	position.y += velocity
 
-func initialize(x_position : float):
+func initialize(x_position : float, id: int):
 	height = position.y
 	target_height = position.y
 	velocity = 0
 	position.x = x_position
+	index = id
+
+func set_collision_width(value):
+	var size : Vector2 = collision_shape_2d.shape.get_size()
+	var new_extents = Vector2(value, size.y)
+	collision_shape_2d.shape.set_size(new_extents)
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	
+	if body == collided_with : return
+	collided_with = body
+	var speed = body.velocity.y * motion_factor
+	emit_signal("splash", index, speed)
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	collided_with = null
